02f591c5c0cb5c8f85dded9d5c315a33ef18609d
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-06-28 21:10:44 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,23 +1,24 @@
 import sys
-from settings import*
+from settings import *
 
-id = sys.argv [1]
-url = sys.argv [2]
-create_json (id = id, status = "running", url = url)
+id = sys.argv[1]
+url = sys.argv[2]
+create_json(id=id, status="running", url=url)
 
-if len (sys.argv) == 3:
+if len(sys.argv) == 3:
     try:
-        hash_sum = md5_hash_sum (url)
-        create_json (id = id, status = "done", url = url,  md5 = hash_sum)
+        hash_sum = md5_hash_sum(url)
+        create_json(id=id, status="done", url=url, md5=hash_sum)
     except:
-        create_json (id = id, status = "failed", url = url)
-        sys.exit ()
-elif len (sys.argv) == 4:
+        create_json(id=id, status="failed", url=url)
+        sys.exit()
+        
+elif len(sys.argv) == 4:
     try:
-        email = sys.argv [3]
-        hash_sum = md5_hash_sum (url)
+        email = sys.argv[3]
+        hash_sum = md5_hash_sum(url)
         send_email(email, url + "\n" + hash_sum)
-        create_json (id = id, status = "done", url = url, md5 = hash_sum, email = email)
+        create_json(id=id, status="done", url=url, md5=hash_sum, email=email)
     except:
-        create_json (id = id, status = "failed", url = url)
-        sys.exit ()
+        create_json(id=id, status="failed", url=url)
+        sys.exit()
