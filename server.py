ff5782577d7d61f534a241e69ea236c3f3aea757
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-06-28 17:23:53 +0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import *
+import uuid
+import subprocess
+
+app = Flask (__name__)
+
+def check_json (id):
+    try:
+        with open (id + ".json") as ouf:
+            task = json.load (ouf)
+    except:
+        return {"status" : "not exists"}
+    status = task ['status']
+    if status == "running":
+        return {"status" : "running"}
+    elif status == "failed":
+        return {"status" : "failed"}
+    else:
+        md5 = task ['md5']
+        url = task ['url']
+        return {"md5" : md5, "status" : "done", "url" : url}
+
+@app.route('/submit', methods=['POST'])
+def create_task ():
+    url = request.form ['url']
+    id = str (uuid.uuid4 ())
+    try:
+        email = request.form ['email']
+        subprocess.Popen (["python", "application.py", str(id), str(url), str(email)])
+    except:
+        subprocess.Popen (["python", "application.py", str(id), str(url)])
+    return jsonify ({'id' : id})
+
+@app.route('/check', methods=['GET'])
+def get_task ():
+    id = request.args.get ('id')
+    return jsonify (check_json (id))
+
+if __name__ == '__main__':
+    app.run (port="8000", debug=True)
---
+++ settings.py
@@ -0,0 +1,49 @@
+import json
+import hashlib
+import requests
+import smtplib
+
+#Функция для подсчета md5 - хеш суммы
+def md5_hash_sum (url):
+    res = requests.get (url)
+    #Поднимаем ошибку, если наш реквест был неудачным
+    try:
+       res.raise_for_status ()
+    except requests.exception.HTTPError as e:
+       return e
+    hash_sum = hashlib.md5()
+    #Используем метод iter_content, так как мы не знаем насколько большие файлы нам были переданы в Url
+    for chunk in res.iter_content (chunk_size = 1000000):
+        if chunk:
+            hash_sum.update (chunk)
+    return hash_sum.hexdigest ()
+
+smtp_server = "smpt.yandex.ru"
+smtp_port = 25
+#Данные для авторизации (можно вставить свою почту и пароль)
+smtp_email = "dankol@yandex.ru"
+smpt_password = "102030"
+
+#Функция для отправки email'а по адресу из нашего POST'а
+def send_email(email, msg):
+    mail_lib = smtplib.SMTP (smtp_server, smtp_port)
+    try:
+        mail_lib.login (smtp_email, smtp_password)
+    except SMTPAuthenticationError:
+       return
+    mail_lib.sendmail (smtp_email, email, msg)
+    mail_lib.quit ()
+
+#Функция создания json файлов в качестве нашей "базы данных"
+def create_json (id, status, url, md5 = 'None', email = 'None'):
+    #Создаем задачу для текущего запроса
+    task = {
+        'id' : id,
+        'url' : url,
+        'status' : status,
+        'md5' : md5,
+        'email' : email
+      }
+    #Создаем файл с именем <id нашего запроса>.json и записываем в него нашу задачу
+    with open (id + ".json", 'w') as inf:
+        json.dump (task, inf, indent=4)
